315b7aebac0ba9641e4ec3ba3d82569ae80a93a0
Benito <benitocavazosr@yahoo.com>
2021-12-17 01:26:34 -0600

    fetch incluiding uvi

--- develop/js/script.js
+++ develop/js/script.js
@@ -4,21 +4,36 @@ var search = function(){
     $("#search").on("click", function(){
         alert("click");
         var text = $("#zipcode").val();
-        console.log(text);
         //https://api.openweathermap.org/data/2.5/weather?q=san%20antonio&APPID=22964cc66fc396ccbca6cca46b9ca9f7
         var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + text + ",US&units=imperial&APPID=22964cc66fc396ccbca6cca46b9ca9f7";
-        var response = fetch(apiUrl)
+        var lat;
+        var lon;
+        var responselat = fetch(apiUrl)
         .then(function(response) {
                 response.json().then(function(data) {
+                    lat = data.coord.lat;
+                    console.log(lat);
+                    lon = data.coord.lon;
+                    console.log(lon);
                     $("#city").text(data.name); 
                     $("#temp").text("Temp : " + data.main.temp +" F");
                     $("#wind").text("Wind : " + data.wind.speed + "MPH");
                     $("#Humidity").text("Humidity : " + data.main.humidity + " %");
-                    $("#uv").text("UV Index : " + data.)
+                    apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +  "&units=imperial&APPID=22964cc66fc396ccbca6cca46b9ca9f7";
+                    var response = fetch(apiUrl)
+                        .then(function(response) {
+                            response.json().then(function(data) {
+                   
+                                $("#uv").text("UV Index : " + data.current.uvi);
                      
                    
+                            });
+                        });
+                   
+                });
             });
-        });
+    
+        
         
     });
 
