600bd9bbf1375c8cf6f87520f609586c0f412b38
Benito <benitocavazosr@yahoo.com>
2021-12-18 01:41:00 -0600

    showing weather and 5 day forecast

--- develop/js/script.js
+++ develop/js/script.js
@@ -12,9 +12,9 @@ var search = function(){
         .then(function(response) {
                 response.json().then(function(data) {
                     lat = data.coord.lat;
-                    console.log(lat);
+                    
                     lon = data.coord.lon;
-                    console.log(lon);
+                    
                     var date = moment().format('L');
                     
                     // <img src="img_girl.jpg" alt="Girl in a jacket"></img>
@@ -23,6 +23,7 @@ var search = function(){
                     $("#wind").text("Wind : " + data.wind.speed + "MPH");
                     $("#Humidity").text("Humidity : " + data.main.humidity + " %");
                     apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +  "&units=imperial&APPID=22964cc66fc396ccbca6cca46b9ca9f7";
+                    //Fuction to get UVI index
                     var response = fetch(apiUrl)
                         .then(function(response) {
                             response.json().then(function(data) {
@@ -37,7 +38,32 @@ var search = function(){
                                 
                             });
                         });
-                   
+                    //fuction to display fore Cast information 
+                    for(let i =1; i<6 ; i++){
+                        var dateForecast = moment().add(i, 'days').format('L');
+                        var foreCastId = "#foreCast"+i;
+                        $(foreCastId).text(dateForecast);
+                        
+                        
+                        //
+                        fetch(apiUrl)
+                            .then(function(response) {
+                                response.json().then(function(data) {
+                    
+                                    var urlIconForeCast = "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png";
+                                    var imageForeCastId = "#imgForeCast"+i;
+                                    $(imageForeCastId).attr("src", urlIconForeCast);
+                                    var tempForeCastId = "#tempForeCast"+i;
+                                    $(tempForeCastId).text("Temp : " + data.daily[i].temp.day +" F");
+                                    var windForeCastId = "#windForeCast"+i;
+                                    $(windForeCastId).text("Wind : " + data.daily[i].wind_speed + "MPH");
+                                    var HumidityForeCastId = "#HumidityForeCast"+i;
+                                    $(HumidityForeCastId).text("Humidity : " + data.daily[i].humidity + " %");
+                                });
+                            });
+
+                    };
+                    
                 });
             });
     
