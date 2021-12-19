4354620293d35cef65bcd2ae56544a59f8edc03d
Benito <benitocavazosr@yahoo.com>
2021-12-19 01:07:46 -0600

    adding the capability of search history

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,11 +1,43 @@
-var search = function(){
-    console.log("in secrch");
-
+var loadHistory = function(){
+    var info = localStorage.getItem("cityHistory1");
+    console.log ("valor de memoria: " + info);
     
-        alert("click");
-        var text = $("#zipcode").val();
+    if(!info){
+        console.log("en if de !info con info");
+        info = "City";
+        localStorage.setItem("cityHistory1", "City");
+        localStorage.setItem("cityHistory2", "City");
+        localStorage.setItem("cityHistory3", "City");
+        localStorage.setItem("cityHistory4", "City");
+        localStorage.setItem("cityHistory5", "City");
+        localStorage.setItem("cityHistory6", "City");
+        
+
+    }
+    
+    $("#cityHistory1").text(info);
+    info = localStorage.getItem("cityHistory2");
+    $("#cityHistory2").text(info);
+    info = localStorage.getItem("cityHistory3");
+    $("#cityHistory3").text(info);
+    info = localStorage.getItem("cityHistory4");
+    $("#cityHistory4").text(info);
+    info = localStorage.getItem("cityHistory5");
+    $("#cityHistory5").text(info);
+    info = localStorage.getItem("cityHistory6");
+    $("#cityHistory6").text(info);
+    init();
+
+};
+
+var displayWeather = function(city , fromInput ){
+    console.log("in secrch");
+       console.log(fromInput);
+    
+        
+       
         //https://api.openweathermap.org/data/2.5/weather?q=san%20antonio&APPID=22964cc66fc396ccbca6cca46b9ca9f7
-        var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + text + ",US&units=imperial&APPID=22964cc66fc396ccbca6cca46b9ca9f7";
+        var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",US&units=imperial&APPID=22964cc66fc396ccbca6cca46b9ca9f7";
         var lat;
         var lon;
         var getInfo = fetch(apiUrl)
@@ -23,11 +55,34 @@ var search = function(){
                     $("#wind").text("Wind : " + data.wind.speed + "MPH");
                     $("#Humidity").text("Humidity : " + data.main.humidity + " %");
                     apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +  "&units=imperial&APPID=22964cc66fc396ccbca6cca46b9ca9f7";
-                    //Fuction to get UVI index
+                    console.log ("link api de ciudad"+ apiUrl)
+                    //moving city to local History
+                    if(fromInput == 1){
+                        var cityToLocalMemory = localStorage.getItem("cityHistory5");
+                        $("#cityHistory6").text(cityToLocalMemory);
+                        localStorage.setItem("cityHistory6", cityToLocalMemory);
+                        cityToLocalMemory = localStorage.getItem("cityHistory4");
+                        $("#cityHistory5").text(cityToLocalMemory);
+                        localStorage.setItem("cityHistory5", cityToLocalMemory);
+                        cityToLocalMemory = localStorage.getItem("cityHistory3");
+                        $("#cityHistory4").text(cityToLocalMemory);
+                        localStorage.setItem("cityHistory4", cityToLocalMemory);
+                        cityToLocalMemory = localStorage.getItem("cityHistory2");
+                        $("#cityHistory3").text(cityToLocalMemory);
+                        localStorage.setItem("cityHistory3", cityToLocalMemory);
+                        cityToLocalMemory = localStorage.getItem("cityHistory1");
+                        $("#cityHistory2").text(cityToLocalMemory);
+                        localStorage.setItem("cityHistory2", cityToLocalMemory);
+                        $("#cityHistory1").text(city);
+                        localStorage.setItem("cityHistory1", city);
+                    };    
+                   
+
+                    //code to get UVI index
                     var response = fetch(apiUrl)
                         .then(function(response) {
                             response.json().then(function(data) {
-                   
+                               
                                 $("#uv").text("UV Index : " + data.current.uvi);
 
                                 console.log(data.current.uvi)
@@ -38,7 +93,7 @@ var search = function(){
                                 
                             });
                         });
-                    //fuction to display fore Cast information 
+                    //code to display fore Cast information 
                     for(let i =1; i<6 ; i++){
                         console.log( "valor de i " + i)
                         var dateForecast = moment().add(i, 'days').format('L');
@@ -76,11 +131,105 @@ var search = function(){
 
 };
 
+var init = function(){
+    $("#search").on("click", function(){
+        
+        var text = $("#zipcode").val();
+        if(text){
+            console.log("en if de Init ")
+            displayWeather (text , "1");
+        }else{
+        alert("Please type a City");
+        };
+        
+
+    });
+   
+
+};
 
 // search();
 
-$("#search").on("click", search);
-//  function(){
+loadHistory();
 
-// });
+$("#cityHistory1").on("click" , function(){
+    var text = $("#cityHistory1").text();
+    
+    if(text  === "City"){
+        alert("Please type a City");
+    }else if(text) {
+        
+        displayWeather(text, "0");
+    };
+    
+} );
+
+
+$("#cityHistory2").on("click" , function(){
+    var text = $("#cityHistory2").text();
+ 
+    if(text  === "City"){
+        alert("Please type a City");
+    }else if(text) {
+        
+        displayWeather(text, "0");
+    };
+    
+} );
+
+
+$("#cityHistory3").on("click" , function(){
+    var text = $("#cityHistory3").text();
+    
+    if(text  === "City"){
+        alert("Please type a City");
+    }else if(text) {
+        
+        displayWeather(text, "0");
+    };
+    
+} );
+
+
+$("#cityHistory4").on("click" , function(){
+    var text = $("#cityHistory4").text();
+    
+    if(text  === "City"){
+        alert("Please type a City");
+    }else if(text) {
+        
+        displayWeather(text, "0");
+    };
+    
+} );
+
+
+$("#cityHistory5").on("click" , function(){
+    var text = $("#cityHistory5").text();
+    
+    if(text  === "City"){
+        alert("Please type a City");
+    }else if(text) {
+        
+        displayWeather(text, "0");
+    };
+    
+} );
+
+
+$("#cityHistory6").on("click" , function(){
+    var text = $("#cityHistory6").text();
+    
+    if(text  === "City"){
+        alert("Please type a City");
+    }else if(text) {
+        
+        displayWeather(text, "0");
+    };
+    
+} );
+
+
+
+
 
